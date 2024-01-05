5b44645aa2aae4c950dc5086b306832e8a74b44a
Billy Arredondo <billyarredondo.coder@gmail.com>
2024-01-05 00:41:01 -0500

    model user

---
+++ gubiarpa.prosegur-coffee.web-api/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace gubiarpa.prosegur_coffee.web_api.Models
+{
+    public class User
+    {
+        public Guid IDUser { get; set; }
+        public string DocumentNumber { get; set; }
+        public string Fullname { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
