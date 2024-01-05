2935396bc927039dfe87c3614065538926d14df5
Billy Arredondo <billyarredondo.coder@gmail.com>
2024-01-05 00:44:19 -0500

    create db context

---
+++ gubiarpa.prosegur-coffee.web-api/Data/CoffeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using gubiarpa.prosegur_coffee.web_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace gubiarpa.prosegur_coffee.web_api.Data
+{
+    public class CoffeeDbContext : DbContext
+    {
+        public CoffeeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
