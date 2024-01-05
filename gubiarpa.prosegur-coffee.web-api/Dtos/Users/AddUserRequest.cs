e8ce150d9b3b90bfd3128013fbf6810e6cdcca38
Billy Arredondo <billyarredondo.coder@gmail.com>
2024-01-05 01:39:14 -0500

    create user request

---
+++ gubiarpa.prosegur-coffee.web-api/Dtos/Users/AddUserRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace gubiarpa.prosegur_coffee.web_api.Dtos.Users
+{
+    public class AddUserRequest
+    {
+        public string DocumentNumber { get; set; }
+        public string Fullname { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
